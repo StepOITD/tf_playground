68e961d52f206d61188f5b7977aea056f04b2ac3
repaint <1194479264@qq.com>
2018-03-27 11:09:17 +0800

    init ex2

--- DeepKMeans/CNN_KMeans_Maximize_Distance.py
+++ DeepKMeans/CNN_KMeans_Maximize_Distance.py
@@ -21,7 +21,8 @@ def gen_matrix(code, centroids):
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
+    # initial = tf.truncated_normal(shape, stddev=0.1)
+    initial = tf.random_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -49,27 +50,31 @@ def inference(mnist):
 
     global_step = tf.Variable(0, trainable=False)
 
-    W_conv1 = weight_variable([5, 5, 1, 32])
-    b_conv1 = bias_variable([32])
+    W_conv1 = weight_variable([3, 3, 1, 128])
+    b_conv1 = bias_variable([128])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([5, 5, 32, 64])
-    b_conv2 = bias_variable([64])
+    W_conv2 = weight_variable([3, 3, 128, 100])
+    b_conv2 = bias_variable([100])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_conv3 = weight_variable([3, 3, 64, 128])
-    b_conv3 = bias_variable([128])
-    h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
-    h_pool3 = max_pool_2x2(h_conv3)
+    # W_conv3 = weight_variable([3, 3, 128, 64])
+    # b_conv3 = bias_variable([64])
+    # h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
+    # h_pool3 = max_pool_2x2(h_conv3)
 
-    W_conv4 = weight_variable([3, 3, 128, 256])
-    b_conv4 = bias_variable([256])
-    h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4) + b_conv4)
-    h_pool4 = max_pool_2x2(h_conv4)
+    # W_conv4 = weight_variable([5, 5, 64, 32])
+    # b_conv4 = bias_variable([32])
+    # h_conv4 = tf.nn.relu(conv2d(h_pool3, W_conv4) + b_conv4)
+    # h_pool4 = max_pool_2x2(h_conv4)
 
-    h_pool4_flat = tf.reshape(h_pool2, [-1, 2*2*256])
+    h_pool4_flat = tf.reshape(h_pool2, [-1, 7*7*100])
+
+    # gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.4)
+    # config = tf.ConfigProto(gpu_options=gpu_options)
+    # config.gpu_options.allow_growth = True
 
     with tf.Session() as sess:
         tf.global_variables_initializer().run()
@@ -81,13 +86,14 @@ def inference(mnist):
         immean = immean.flatten()
         imnbr = 10000
         projected = array([dot(V[:32], extract_features[i] - immean) for i in range(imnbr)])
-        centroids, distortion = kmeans(projected, 15)
+        centroids, distortion = kmeans(projected, 20)
 
         code, distance = vq(projected, centroids)
 
-        tools.calculate_acc(code, ys, 10)
+        tools.calculate_acc(code, ys, 10, 20)
 
     return extract_features
+
 
 mnist = input_data.read_data_sets("mnist_data", one_hot=False)
 pre = inference(mnist)
